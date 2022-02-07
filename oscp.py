1d3550dbcbf1a51b62b4799ac8898bd9f7a069cb
Muttakin-shikder <99129135+Muttakin-Shikder@users.noreply.github.com>
2022-02-07 17:01:14 +0600

    Add files via upload

---
+++ oscp.py
@@ -0,0 +1,30 @@
+
+#first github project.
+
+import os
+os.system('clear')
+
+
+a = eval(input("enter: "))
+b = eval(input("enter: "))
+
+p = a+b
+m = a-b
+d = a/b
+fd = a//b
+mo = a%b
+Mul =a*b
+
+print("---------------------all calculation.---------------------")
+
+print("Addition(+): ",p)
+print("Subtraction(-): ",m)
+print("Division(/): ",d)
+print("Floor division(//): ",fd)
+print("Division's reminder(%): ",mo)
+print("Multiplication(x): ",Mul)
+
+
+
+
+
